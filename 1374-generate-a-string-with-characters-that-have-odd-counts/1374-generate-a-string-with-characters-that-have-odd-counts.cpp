9d4ed6e32147ef0b621d1cbeb15bef378ef64b6e
Krishna Tiwari <krishna.08072005@gmail.com>
2026-07-03 01:11:46 +0530

    Time: 0 ms (100%), Space: 8.9 MB (62.31%) - LeetHub

---
+++ 1374-generate-a-string-with-characters-that-have-odd-counts/1374-generate-a-string-with-characters-that-have-odd-counts.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string generateTheString(int n) {
+        string ans = "";
+        for (int i = 0; i < n - 1; i++) {
+            ans += 'a';
+        }
+        if (n % 2 == 0) {
+            return ans + 'b';
+        }
+        return ans + 'a';
+    }
+};
